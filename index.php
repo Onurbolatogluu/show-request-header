af9419f5d604463039e9330289a74fb0af45db20
Onurbolatogluu <bolatogluu@gmail.com>
2023-05-16 19:15:53 +0300

    changed index.php

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
+$planets = array("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto", "Ceres", "Eris", "Haumea", "Makemake"); // Rastgele gezegen isimleri dizisi
+$random_planet = $planets[array_rand($planets)]; // Diziden rastgele bir gezegen seçme
 foreach (getallheaders() as $name => $value) {
     echo $name . ": " . $value . "<br>";
 }
 
 echo "<br>";
-echo "Tarih ve Zaman Yeni Yesyeni yenillldddddl   ahanda bitti finishiiiiiiiiiiiiiiiii   gsfbtsbdsadasdasdasdasdsaasssssssssdfjfj: " . date("Y-m-d H:i:s") . "<br>";
-
+echo "Tarih ve Zaman: " . date("Y-m-d H:i:s") . "<br>";
+echo "Rastgele Gezegen: " . $random_planet . "<br>"; // Rastgele gezegeni yazdırma
 ?>
+
+
